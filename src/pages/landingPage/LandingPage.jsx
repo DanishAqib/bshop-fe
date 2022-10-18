9d80fa69fc9acee13fa72c4b12a242b8b42200d8
Danish Aqib <danishaqib129@gmail.com>
2022-10-18 08:24:46 +0500

    modified landing page

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -7,8 +7,16 @@ export const LandingPage = () => {
     <>
     <div className="main">
       <div className="bodyContainer">
-        <Barber />
-        <Customer />
+        <div className="bodyContainer__left">
+          <h2>
+            Welcome to <span>BookBarber</span>
+          </h2>
+          <p>Please select your role</p>
+        </div>
+        <div className="bodyContainer__right">
+          <Barber />
+          <Customer />
+        </div>
       </div>
     </div>
     </>
